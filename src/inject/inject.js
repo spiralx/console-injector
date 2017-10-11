61421bdbb589b99736303f8fbfe5318becdeae51
James Skinner <spiralx@gmail.com>
2017-10-11 18:33:50 +0100

    Added StandardJS-based linting and tided code to pass

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,53 +1,49 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
 
+chrome.extension.sendMessage({}, function (response) {
+  var readyStateCheckInterval = setInterval(function () {
+    if (document.readyState === 'complete') {
+      clearInterval(readyStateCheckInterval)
 
-    var element = document.createElement('script');
+      var element = document.createElement('script')
 
-    var injectFunction = function (library) {
-      
-      function getURLs() {
-        var xmlhttp = new XMLHttpRequest();
+      var injectFunction = function (library) {
+        function getURLs () {
+          var xmlhttp = new XMLHttpRequest()
 
-        xmlhttp.onreadystatechange = function() {
-          if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
-            if(xmlhttp.status == 200){
-              var libraries = JSON.parse(xmlhttp.responseText).results;
-              var foundLib = libraries.reduce(function (found, item) {
-                if (item.name === library || item.name === library + '.js') {
-                  found = item;
-                } 
-                return found;
-              }, undefined);
+          xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState === XMLHttpRequest.DONE) {
+              if (xmlhttp.status === 200) {
+                var libraries = JSON.parse(xmlhttp.responseText).results
+                var foundLib = libraries.reduce(function (found, item) {
+                  if (item.name === library || item.name === library + '.js') {
+                    found = item
+                  }
+                  return found
+                }, undefined)
 
-              if (foundLib) {
-                var url = foundLib.latest.replace('http:', 'https:');
-                var libScript =document.createElement('script');
-                libScript.src = url;
-                document.head.appendChild(libScript);
-                return console.log('library injected from ' + url);
-              } else {
-                console.log('library "' + library + '" not found');
-              }
+                if (foundLib) {
+                  var url = foundLib.latest.replace('http:', 'https:')
+                  var libScript = document.createElement('script')
+                  libScript.src = url
+                  document.head.appendChild(libScript)
+                  return console.log('library injected from ' + url)
+                } else {
+                  console.log('library "' + library + '" not found')
+                }
+              } else { console.log(xmlhttp.status) }
             }
-            else { console.log(XMLHttpRequestlhttp.status)}
           }
+
+          var searchString = 'https://api.cdnjs.com/libraries?search=' + library
+          xmlhttp.open('GET', searchString, true)
+          xmlhttp.send()
         }
 
-        var searchString = 'https://api.cdnjs.com/libraries?search=' + library;
-        xmlhttp.open("GET", searchString, true);
-        xmlhttp.send();
+        getURLs()
       }
 
-      getURLs();
+      element.innerHTML = 'console.inject = ' + injectFunction.toString()
+      document.head.appendChild(element)
     }
-
-    element.innerHTML = 'console.inject = ' + injectFunction.toString();
-    document.head.appendChild(element);
-
-
-	}
-	}, 10);
-});
+  }, 10)
+})
---
+++ src/popup/popup.js
@@ -0,0 +1,21 @@
+
+// eslint-disable-next-line no-unused-vars
+function ci (library) {
+  const libraryUrls = {
+    'angular': 'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.5/angular.min.js',
+    'jquery': 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js',
+    'jquery1': 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js',
+    'three.js': 'https://ajax.googleapis.com/ajax/libs/threejs/r69/three.min.js',
+    'threejs': 'https://ajax.googleapis.com/ajax/libs/threejs/r69/three.min.js',
+    'd3': 'https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.3/d3.min.js',
+    'underscore': 'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore-min.js',
+    'prototype': 'https://ajax.googleapis.com/ajax/libs/prototype/1.7.2.0/prototype.js',
+    'dojo': 'https://ajax.googleapis.com/ajax/libs/dojo/1.10.3/dojo/dojo.js',
+    'backbone': 'https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min.js'
+  }
+
+  const lib = document.createElement('script')
+  lib.src = libraryUrls[ library.toLowerCase() ]
+
+  document.head.appendChild(lib)
+}
