4efbf8b4324a5c910e2529e5ee0f9a97e426313c
James Skinner <spiralx@gmail.com>
2017-10-16 23:32:32 +0100

    Changed to inject from unpkg.com

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,49 +1,59 @@
+function inject (library) {
+  function getURLs () {
+    var xmlhttp = new XMLHttpRequest()
 
-chrome.extension.sendMessage({}, function (response) {
-  var readyStateCheckInterval = setInterval(function () {
-    if (document.readyState === 'complete') {
-      clearInterval(readyStateCheckInterval)
-
-      var element = document.createElement('script')
-
-      var injectFunction = function (library) {
-        function getURLs () {
-          var xmlhttp = new XMLHttpRequest()
-
-          xmlhttp.onreadystatechange = function () {
-            if (xmlhttp.readyState === XMLHttpRequest.DONE) {
-              if (xmlhttp.status === 200) {
-                var libraries = JSON.parse(xmlhttp.responseText).results
-                var foundLib = libraries.reduce(function (found, item) {
-                  if (item.name === library || item.name === library + '.js') {
-                    found = item
-                  }
-                  return found
-                }, undefined)
-
-                if (foundLib) {
-                  var url = foundLib.latest.replace('http:', 'https:')
-                  var libScript = document.createElement('script')
-                  libScript.src = url
-                  document.head.appendChild(libScript)
-                  return console.log('library injected from ' + url)
-                } else {
-                  console.log('library "' + library + '" not found')
-                }
-              } else { console.log(xmlhttp.status) }
+    xmlhttp.onreadystatechange = function () {
+      if (xmlhttp.readyState === XMLHttpRequest.DONE) {
+        if (xmlhttp.status === 200) {
+          var libraries = JSON.parse(xmlhttp.responseText).results
+          var foundLib = libraries.reduce(function (found, item) {
+            if (item.name === library || item.name === library + '.js') {
+              found = item
             }
+            return found
+          }, undefined)
+
+          if (foundLib) {
+            var url = foundLib.latest.replace('http:', 'https:')
+            var libScript = document.createElement('script')
+            libScript.src = url
+            document.head.appendChild(libScript)
+            return console.log('library injected from ' + url)
+          } else {
+            console.log('library "' + library + '" not found')
           }
-
-          var searchString = 'https://api.cdnjs.com/libraries?search=' + library
-          xmlhttp.open('GET', searchString, true)
-          xmlhttp.send()
-        }
-
-        getURLs()
+        } else { console.log(xmlhttp.status) }
       }
-
-      element.innerHTML = 'console.inject = ' + injectFunction.toString()
-      document.head.appendChild(element)
     }
-  }, 10)
-})
+
+    var searchString = 'https://api.cdnjs.com/libraries?search=' + library
+    xmlhttp.open('GET', searchString, true)
+    xmlhttp.send()
+  }
+
+  getURLs()
+}
+
+async function inject2 (library) {
+  const url = `https://unpkg.com/${library}`
+
+  try {
+    const response = await fetch(url)
+
+    const se = document.createElement('script')
+    se.type = 'text/javascript'
+    se.src = url
+
+    document.body.appendChild(se)
+
+    console.info(`Library "${library}" loaded from ${response.url}`)
+  } catch (ex) {
+    console.warn(`Library "${library}" not found at ${url}`)
+  }
+}
+
+// ------------------------------------------------------------
+
+const element = document.createElement('script')
+element.innerHTML = 'console.inject = ' + inject2.toString()
+document.head.appendChild(element)
